53dea18d1e5a50213fa3bbe8f4c6c4af8dd7c2b9
Regis Grundig <reginaldo.venturadesa@gmail.com>
2015-08-14 20:28:07 -0300

    script eta 40

--- adquire_eta40.sh
+++ adquire_eta40.sh
@@ -37,6 +37,9 @@ export LANG=en_us_8859_1
 
 #
 # verifica sistema
+# no cygwin (windows) 
+# se bem instalado deve
+# funcionar sem as variaveis
 #
 MACH=`uname -a | cut -c1-5` 
 if [ $MACH = "Linux" ];then 
@@ -71,13 +74,13 @@ fi
 #
 # verifica se existe diretorio SAIDA. se não cria.
 #
-if [ ! -f ./SAIDAS ] ; then 
+if [ ! -f ./SAIDAS ];then 
 mkdir ./SAIDAS            >./LOG.prn 2>&1 
 fi  
 # entra no direotiro SAIDA e depois diretorio da data do dia
 # onde tudo aocntece. 
 cd SAIDAS
-mkdir $data$hora    >./LOG.prn 2>&1 
+mkdir $data$hora    >>./LOG.prn 2>&1 
 cd $data$hora
 
 echo "["`date`"] BAIXANDO DADOS" 
@@ -95,12 +98,12 @@ wget -nc ftp://ftp1.cptec.inpe.br/modelos/io/tempo/regional/Eta40km_ENS/prec24/$
 # por isso vou juntar todos os .bin num único do arquivo
 #
 echo "["`date`"] CRIANDO ARQUIVOS PARA O GRADS" 
-rm $data$hora".bin"
-rm *.ctl
+rm $data$hora".bin" >>./LOG.prn 2>&1 
+rm *.ctl            >>./LOG.prn 2>&1   
 for file in `ls -1 *.bin`
 do
-cat $file >>  $data$hora".bin"
-rm $file
+cat $file >>  $data$hora".bin"     
+rm $file                            
 done
 file=`echo $data$hora".bin"`
 #
@@ -129,15 +132,22 @@ echo "ENDVARS" >>modelo_all.ctl
 
 #
 #  cria o script para a criacao de figuras de data corrida 
+#  de 7 dias a frente 
 #
-echo "'open modelo_all.ctl'"                                  >figura.gs
-echo "'set mpdset hires'"                                    >>figura.gs
-echo "'../../cores.gs'"                                         >>figura.gs
-echo "'set gxout shaded'"                                    >>figura.gs
-echo "'d sum(prec,t=2,t=8)'"                                 >>figura.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura.gs
-echo "'draw string 0.5 8.1 RODADA :"$DATA0" - "$hora"Z'"     >>figura.gs
-echo "'draw string 0.5 7.9 Periodo:"$DATA2" a "$DATA1"'"     >>figura.gs
+echo "*"                                                              >figura.gs
+echo "* esse script é auto gerado. documentação em adquire_eta.sh"   >>figura.gs
+echo "* By reginaldo.venturadesa@gmail.com   "                       >>figura.gs
+echo "'open modelo_all.ctl'"                                         >>figura.gs
+echo "'set parea 0.5 8.5 1.5 10.2'"                                  >>figura.gs
+echo "'set lon -75 -30'"                                             >>figura.gs
+echo "'set lat -35  10'"                                             >>figura.gs
+echo "'set mpdset hires'"                                            >>figura.gs
+echo "'../../cores.gs'"                                              >>figura.gs
+echo "'set gxout shaded'"                                            >>figura.gs
+echo "'d sum(prec,t=2,t=8)'"                                         >>figura.gs
+echo "'draw string 0.5 10.8 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura.gs
+echo "'draw string 0.5 10.6 RODADA :"$DATA0" - "$hora"Z'"     >>figura.gs
+echo "'draw string 0.5 10.4 Periodo:"$DATA2" a "$DATA1"'"     >>figura.gs
 echo "'cbarn.gs'"                                            >>figura.gs
 echo "'../../plota.gs'"                                         >>figura.gs
 echo "'printim prec07dias_"$data"_"$hora"Z.png white'"       >>figura.gs
@@ -145,10 +155,22 @@ echo "'quit'"                                                >>figura.gs
 #
 #  cria o script para data operativa
 #
-echo "'open modelo_all.ctl'"            >figura2.gs
-echo "'set mpdset hires'"               >>figura2.gs
-echo "'set gxout shaded'"               >>figura2.gs
-echo "t0=10"                            >>figura2.gs  
+echo "*"                                                              >figura2.gs
+echo "* esse script é auto gerado. documentação em adquire_eta.sh"   >>figura2.gs
+echo "*By reginaldo.venturadesa@gmail.com "                           >>figura2.gs
+echo "'open modelo_all.ctl'"                     >figura2.gs
+echo "'set parea off'"                     >>figura2.gs
+echo "'set parea 0.5 8.5 1.5 10.0'"                                  >>figura2.gs
+echo "'set lon -75 -30'"                                  >>figura2.gs
+echo "'set lat -35  10'"                                  >>figura2.gs
+echo "'set mpdset hires'"                       >>figura2.gs
+echo "'set gxout shaded'"                       >>figura2.gs
+echo "'set string 1 c'"                       >>figura2.gs
+#
+# acha a data que tem o sabado que é o marco
+# para a semana operativa
+#
+echo "t0=10"                                                                        >>figura2.gs  
 echo "'set t 2 last'"                   >>figura2.gs
 echo "'q time'"                         >>figura2.gs
 echo "var3=subwrd(result,5)"            >>figura2.gs
@@ -163,6 +185,10 @@ echo "endif"                            >>figura2.gs
 echo "tt=tt+1"                          >>figura2.gs
 echo "endwhile"                         >>figura2.gs
 echo "say t0"                           >>figura2.gs
+
+#
+# plota semana operativa 1
+#
 echo "'set t 2 't0"                     >>figura2.gs
 echo "'q time'"                         >>figura2.gs
 echo "var1=subwrd(result,3)"            >>figura2.gs
@@ -172,34 +198,48 @@ echo "'set t 1'"                        >>figura2.gs
 echo "'../../cores.gs'"                 >>figura2.gs
 echo "'d sum(prec,t=2,t='t0')'"         >>figura2.gs
 echo "'cbarn.gs'"                       >>figura2.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1'"  >>figura2.gs
-echo "'draw string 0.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura2.gs
-echo "'draw string 0.5 7.9 PERIODO:'var1' a 'var2"                     >>figura2.gs
+echo "'draw string 2.5 10.8 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1 '"  >>figura2.gs
+echo "'draw string 1.5 10.6 RODADA:"$DATA0" - "$hora"Z'"                >>figura2.gs
+echo "'draw string 2.2 10.4 PERIODO:'var1' a 'var2"                     >>figura2.gs                                            
 echo "'../../plota.gs'"                                                   >>figura2.gs
 echo "'printim semanaoperativa_1_"$data".png white'"                       >>figura2.gs
+#
+# plota semana operativa 2
+#
 echo "'c'"                                                             >>figura2.gs
 echo "'../../cores.gs'"                    >>figura2.gs
 echo "'d sum(prec,t='t0',t=10)'"         >>figura2.gs
 echo "'cbarn.gs'"                       >>figura2.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '"  >>figura2.gs
-echo "'draw string 0.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura2.gs
-echo "'draw string 0.5 7.9 PERIODO:'var2' a 'var3"                     >>figura2.gs
+echo "'draw string 2.5 10.8 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '"  >>figura2.gs
+echo "'draw string 1.5 10.6 RODADA:"$DATA0" - "$hora"Z'"                >>figura2.gs
+echo "'draw string 2.2 10.4 PERIODO:'var2' a 'var3"                     >>figura2.gs
 echo "'../../plota.gs'"                                                   >>figura2.gs
 echo "'printim semanaoperativa_2_"$data".png white'"                       >>figura2.gs
 echo "say t0"                           >>figura2.gs
 echo "'quit'"                          >>figura2.gs
 
 
+
+
+#-----------------------------------------------------------------------------------------
+#  cria o script para data operativa por bacia cadastrada
+#  as bacias estao cadastradas em CADASTRO/CADASTRADAS
+# ver documentacao para maiores detalhes
 #
-#  cria o script para data operativa
-#
-echo "'open modelo_all.ctl'"            >figura3.gs
+echo "*"                                                              >figura3.gs
+echo "* esse script é auto gerado. documentação em adquire_eta.sh"   >>figura3.gs
+echo "*By reginaldo.venturadesa@gmail.com "                             >>figura3.gs
+echo "'open modelo_all.ctl'"            >>figura3.gs
 echo "'set mpdset hires'"               >>figura3.gs
 echo "'set gxout shaded'"               >>figura3.gs
 echo "'q gxinfo'"   >>figura3.gs
 echo "var=sublin(result,2)"  >>figura3.gs
 echo "page=subwrd(var,4)" >>figura3.gs
 echo "say page" >>figura3.gs
+echo "if (page ="8.5") " >>figura3.gs
+echo "'set parea 0.5 8.5 1.5 10.2'" >>figura3.gs
+echo "'set parea 0.5 8.5 1.5 10.2'" >>figura3.gs
+echo "endif"                                  >>figura3.gs
 #echo "return" >>figura3.gs
 echo "t0=10"                            >>figura3.gs  
 echo "'set t 2 last'"                   >>figura3.gs
@@ -255,73 +295,83 @@ echo "x1=subwrd(linha,4)"       >>figura3.gs
 echo "y0=subwrd(linha,5)"       >>figura3.gs
 echo "y1=subwrd(linha,6)"       >>figura3.gs
 echo "tipo=subwrd(linha,7)"     >>figura3.gs
+echo "plota=subwrd(linha,8)"     >>figura3.gs
 echo "'set lon 'x1' 'x0 "       >>figura3.gs
 echo "'set lat 'y1' 'y0 "       >>figura3.gs
 
-echo "if (tipo = "RETRATO" & page ="8.5" ) "   >>figura3.gs
+echo "if (tipo = "RETRATO" & page ="8.5" & plota="SIM") "   >>figura3.gs
 echo "'c'"                        >>figura3.gs
+echo "'set parea 0.5 8.5 1.5 10.2'"                                  >>figura3.gs
 echo "'set t 1'"                        >>figura3.gs
 echo "'../../cores.gs'"                    >>figura3.gs
 echo "'d sum(prec,t=2,t='t0')'"         >>figura3.gs
 echo "'cbarn.gs'"                       >>figura3.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1'"  >>figura3.gs
-echo "'draw string 0.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
-echo "'draw string 0.5 7.9 PERIODO:'dia1'/'mes1'/'ano1' a 'dia2'/'mes2'/'ano2  "                     >>figura3.gs
+echo "'draw string 2.5 10.8 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1'"  >>figura3.gs
+echo "'draw string 2.5 10.6 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
+echo "'draw string 2.5 10.4 PERIODO:'dia1'/'mes1'/'ano1' a 'dia2'/'mes2'/'ano2  "                     >>figura3.gs
 echo "'draw shp ../../CONTORNOS/SHAPES/'shape"                                                  >>figura3.gs
 echo "say shape" >>figura3.gs
 echo "'printim 'bacia'_semanaoperativa_1_"$data".png white'"                       >>figura3.gs
 echo "'c'"                                                             >>figura3.gs
+echo "'set parea 0.5 8.5 1.5 10.2'"                                  >>figura3.gs
 echo "'../../cores.gs'"                                                >>figura3.gs
 echo "'d sum(prec,t='t0',t=10)'"                                       >>figura3.gs
 echo "'cbarn.gs'"                                                      >>figura3.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '">>figura3.gs
-echo "'draw string 0.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
-echo "'draw string 0.5 7.9 PERIODO:'dia2'/'mes2'/'ano2' a 'dia3'/'mes3'/'ano3  "                     >>figura3.gs
+echo "'draw string 2.5 10.8 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '">>figura3.gs
+echo "'draw string 2.5 10.6 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
+echo "'draw string 2.5 10.4 PERIODO:'dia2'/'mes2'/'ano2' a 'dia3'/'mes3'/'ano3  "                     >>figura3.gs
 echo "'draw shp ../../CONTORNOS/SHAPES/'shape"                                                        >>figura3.gs
 echo "'printim 'bacia'_semanaoperativa_2_"$data".png white'"                       >>figura3.gs
 echo "'c'"   >>figura3.gs
+echo "'set parea 0.5 8.5 1.5 10.2'"                                  >>figura3.gs
 echo "'set mpdset hires'"                                    >>figura3.gs
 echo "'../../cores.gs'"                                         >>figura3.gs
 echo "'set gxout shaded'"                                    >>figura3.gs
 echo "'d sum(prec,t=2,t=8)'"                                 >>figura3.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura3.gs
-echo "'draw string 0.5 8.1 RODADA :"$DATA0" - "$hora"Z'"     >>figura3.gs
-echo "'draw string 0.5 7.9 Periodo:"$DATA2" a "$DATA1"'"     >>figura3.gs
+echo "'draw string 2.5 10.8 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura3.gs
+echo "'draw string 2.5 10.6 RODADA :"$DATA0" - "$hora"Z'"     >>figura3.gs
+echo "'draw string 2.5 10.4 Periodo:"$DATA2" a "$DATA1"'"     >>figura3.gs
 echo "'cbarn.gs'"                                            >>figura3.gs
 echo "'../../plota.gs'"                                         >>figura3.gs
 echo "'printim 'bacia'_prec07dias_"$data"_"$hora"Z.png white'"       >>figura3.gs
 echo "say t0"                           >>figura3.gs
 echo "endif"                            >>figura3.gs 
 
-echo "if (tipo = "PAISAGEM" & page ="11" ) "   >>figura3.gs
+
+
+
+echo "if (tipo = "PAISAGEM" & page ="11" & plota="SIM" ) "   >>figura3.gs
 echo "'c'"                        >>figura3.gs
+echo "'set parea 0.5 10.5 1.88392 7.31608'"                     >>figura3.gs
 echo "'set t 1'"                        >>figura3.gs
 echo "'../../cores.gs'"                    >>figura3.gs
 echo "'d sum(prec,t=2,t='t0')'"         >>figura3.gs
 echo "'cbarn.gs'"                       >>figura3.gs
-echo "'draw string 0.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1'"  >>figura3.gs
-echo "'draw string 0.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
-echo "'draw string 0.5 7.9 PERIODO:'dia1'/'mes1'/'ano1' a 'dia2'/'mes2'/'ano2  "                     >>figura3.gs
+echo "'draw string 2.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 1'"  >>figura3.gs
+echo "'draw string 2.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
+echo "'draw string 2.5 7.9 PERIODO:'dia1'/'mes1'/'ano1' a 'dia2'/'mes2'/'ano2  "                     >>figura3.gs
 echo "'draw shp ../../CONTORNOS/SHAPES/'shape"                                                  >>figura3.gs
 echo "say shape" >>figura3.gs
 echo "'printim 'bacia'_semanaoperativa_1_"$data".png white'"                       >>figura3.gs
 echo "'c'"                                                             >>figura3.gs
+echo "'set parea 0.5 10.5 1.88392 7.31608'"                     >>figura3.gs
 echo "'../../cores.gs'"                                                >>figura3.gs
 echo "'d sum(prec,t='t0',t=10)'"                                       >>figura3.gs
 echo "'cbarn.gs'"                                                      >>figura3.gs
-echo "'draw string 0.5 5.8 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '">>figura3.gs
-echo "'draw string 0.5 5.6 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
-echo "'draw string 0.5 5.4 PERIODO:'dia2'/'mes2'/'ano2' a 'dia3'/'mes3'/'ano3  "                     >>figura3.gs
+echo "'draw string 2.5 8.3 PRECIPITACAO ACUMULADA SEMANA OPERATIVA 2 '">>figura3.gs
+echo "'draw string 2.5 8.1 RODADA:"$DATA0" - "$hora"Z'"                >>figura3.gs
+echo "'draw string 2.5 7.9 PERIODO:'dia2'/'mes2'/'ano2' a 'dia3'/'mes3'/'ano3  "                     >>figura3.gs
 echo "'draw shp ../../CONTORNOS/SHAPES/'shape"                                                        >>figura3.gs
 echo "'printim 'bacia'_semanaoperativa_2_"$data".png white'"                       >>figura3.gs
 echo "'c'"   >>figura3.gs
+echo "'set parea 0.5 10.5 1.88392 7.31608'"                     >>figura3.gs
 echo "'set mpdset hires'"                                    >>figura3.gs
 echo "'../../cores.gs'"                                         >>figura3.gs
 echo "'set gxout shaded'"                                    >>figura3.gs
 echo "'d sum(prec,t=2,t=8)'"                                 >>figura3.gs
-echo "'draw string 0.5 5.8 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura3.gs
-echo "'draw string 0.5 5.6 RODADA :"$DATA0" - "$hora"Z'"     >>figura3.gs
-echo "'draw string 0.5 5.4 Periodo:"$DATA2" a "$DATA1"'"     >>figura3.gs
+echo "'draw string 2.5 8.3 PRECIPITACAO ACUMULADA 7 DIAS '"  >>figura3.gs
+echo "'draw string 2.5 8.1 RODADA :"$DATA0" - "$hora"Z'"     >>figura3.gs
+echo "'draw string 2.5 7.9 Periodo:"$DATA2" a "$DATA1"'"     >>figura3.gs
 echo "'cbarn.gs'"                                            >>figura3.gs
 echo "'../../plota.gs'"                                         >>figura3.gs
 echo "'printim 'bacia'_prec07dias_"$data"_"$hora"Z.png white'"       >>figura3.gs
@@ -348,16 +398,18 @@ echo "["`date`"] CALCULANDO MEDIA POR BACIA"
 cp ../../calcula_versao3.gs .
 
 
-echo "["`date`"] PLOTANDO FIGURAS" 
+echo "["`date`"] PLOTANDO FIGURAS 7 DIAS BRASIL" 
 
-grads -pbc "figura.gs"  ##>>./LOG.prn 2>&1
-grads -pbc "figura2.gs"  ##>>./LOG.prn 2>&1
-grads -pbc "figura3.gs"  ##>>./LOG.prn 2>&1
-grads -lbc "figura3.gs"  ##>>./LOG.prn 2>&1
-
-grads -lbc "calcula_versao3.gs" ##>>./LOG.prn 2>&1
-
-
+grads -pbc "figura.gs"  >>./LOG.prn 2>&1
+echo "["`date`"] PLOTANDO FIGURAS SEMANA OPERATIVA BRASIL" 
+grads -pbc "figura2.gs"  >>./LOG.prn 2>&1
+echo "["`date`"] PLOTANDO FIGURAS SEMANA OPERATIVA FORMATO RETRATO POR BACIAS" 
+grads -pbc "figura3.gs"  >>./LOG.prn 2>&1
+echo "["`date`"] PLOTANDO FIGURAS SEMANA OPERATIVA FORMATO PAISAGEM POR BACIAS" 
+grads -lbc "figura3.gs"  >>./LOG.prn 2>&1
+echo "["`date`"] CALCULANDO MÉDIA POR BACIA " 
+grads -lbc "calcula_versao3.gs" >>./LOG.prn 2>&1
+echo "["`date`"] AJUSTANDO CRIAÇÕES " 
 mkdir imagens_semanaoperativa_1  >>./LOG.prn 2>&1
 mkdir imagens_semanaoperativa_2 >>./LOG.prn 2>&1
 mkdir imagens_7dias   >>./LOG.prn 2>&1
